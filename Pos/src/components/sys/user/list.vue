8a1f59d0276798126c6c67c3a2060e2a5ed0e257
徐文斌 <1055409319@qq.com>
2019-01-24 19:51:53 +0800

    后台路由完成

--- Pos/src/components/sys/user/list.vue
+++ Pos/src/components/sys/user/list.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    用户列表
+  <div class="list">
+    <div class="data-table">
+      <el-table :data="" border style="width: 100%">
+        <el-table-column prop="name" label="姓名" width="100"></el-table-column>
+        <el-table-column prop="count" label="用户名" width="58"></el-table-column>
+        <el-table-column prop="price" label="身份" width="58"></el-table-column>
+        <el-table-column label="操作" fixed="right">
+          <template slot-scope="scope">
+            <el-button type="text" size="small" @click="delSingleGoods(scope.row)">删除</el-button>
+            <el-button type="text" size="small" @click="addOrderList(scope.row)">修改</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
