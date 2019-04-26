3f4cb7b7223b018e89b67274d2b0c92ec8d058e9
徐文斌 <1055409319@qq.com>
2019-04-26 10:56:29 +0800

    兼容ie 设置

--- Pos/build/webpack.base.conf.js
+++ Pos/build/webpack.base.conf.js
@@ -2,7 +2,8 @@
 const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+const vueLoaderConfig = require('./vue-loader.conf');
+require('babel-polyfill');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -13,7 +14,7 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: ['babel-polyfill', './src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
