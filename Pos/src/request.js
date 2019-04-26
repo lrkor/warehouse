39370b5cfd502d2df3472848e247a8e7d840cc9f
徐文斌 <1055409319@qq.com>
2019-04-26 10:32:58 +0800

    axios 封装

---
+++ Pos/src/request.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+axios.defaults.timeout = 5000;
+axios.defaults.baseURL ='';
+
+
+/**
+ * 封装get方法
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function axiosGet(url,params={}){
+  return new Promise((resolve,reject) => {
+    axios.get(url,{
+      params:params
+    })
+      .then(response => {
+        if(response.data.code=='200'){
+          resolve(response.data);
+        }else {
+          this.$message({
+            message: response.data.message,
+            type: 'warning'
+          });
+        }
+      })
+      .catch(error => {
+        this.$message({
+          message: '网络错误!',
+          type: 'error'
+        });
+        reject(error)
+      })
+  })
+}
+
+/**
+ * 封装post请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function axiosPost(url,data = {}){
+  return new Promise((resolve,reject) => {
+    axios.post(url,data)
+      .then(response => {
+        if(response.data.code=='200'){
+          resolve(response.data);
+        }else {
+          this.$message({
+            message: response.data.message,
+            type: 'warning'
+          });
+        }
+      },err => {
+        this.$message({
+          message: '网络错误!',
+          type: 'error'
+        });
+        reject(err)
+      })
+  })
+}
